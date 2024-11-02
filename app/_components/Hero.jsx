c2b38b0cbf767c662f58553d2cdfbb6406ca8f5e
nithishp <nithishj03@gmail.com>
2024-11-02 09:56:15 +0530

    first commit

---
+++ app/_components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='min-h-screen w-screen bg-gradient-to-t from-red-200 to-white' >
+        <div className='p-10'>
+            <h3 className='text-green-700 uppercase font-semibold text-xl' >india's #1 job platform</h3>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ app/_components/Navbar.jsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Flower2, Menu } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+
+const Navbar = () => {
+  const menus = [
+    { title: "Home", path: "/" },
+    { title: "About", path: "/about" },
+    { title: "Help", path: "/help" },
+  ];
+
+  return (
+    <header className="p-2 shadow text-gray-800 w-full h-max">
+      <div className="flex justify-between">
+        <div className="flex">
+          <Link
+            href="/"
+            aria-label="Back to homepage"
+            className=" flex md:justify-center justify-start items-center"
+          >
+            <Image
+              src="/dreamcompany-logo.jpg"
+              alt="Dream company"
+              width={50}
+              height={30}
+              className="h-10 w-auto"
+            />
+          </Link>
+
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 hidden`}
+          >
+            <ul className="justify-center items-center space-y-8 md:flex md:space-x-6 md:space-y-0 mt-2 ml-5">
+              {menus.map((item, idx) => (
+                <li
+                  key={idx}
+                  className="text-gray-600 hover:text-rose-600 text-base"
+                >
+                  <Link href={item.path}>{item.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="items-center flex-shrink-0 hidden lg:flex">
+          <Button className="px-8 py-2 font-semibold rounded hover:bg-rose-600 text-gray-50">
+            Log in
+          </Button>
+        </div>
+        <div className="md:hidden">
+          <Sheet>
+            <SheetTrigger>
+              <button
+                className="p-2 lg:hidden flex md:justify-center justify-start"
+                title="menu"
+                type="button"
+              >
+                <Menu />
+              </button>
+            </SheetTrigger>
+            <SheetContent className="rounded-l-3xl">
+              <Image
+                src="/dreamcompany-logo.jpg"
+                alt="Dream company"
+                width={50}
+                height={30}
+                className="h-10 w-auto"
+              />
+              <div
+                className={`flex-1 justify-self-start pb-3 mt-8 md:block md:pb-0 md:mt-0`}
+              >
+                <ul className="justify-center items-center space-y-8 md:flex md:space-x-6 md:space-y-0 mt-2">
+                  {menus.map((item, idx) => (
+                    <li
+                      key={idx}
+                      className="text-gray-600 hover:text-rose-600 text-base"
+                    >
+                      <Link href={item.path}>{item.title}</Link>
+                    </li>
+                  ))}
+                  <li>
+                    <Button className="px-8 py-2 w-full font-semibold rounded hover:bg-rose-600 text-gray-50">
+                      Log in
+                    </Button>
+                  </li>
+                </ul>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
